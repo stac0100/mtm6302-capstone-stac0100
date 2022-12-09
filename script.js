a3616bb93c8121e1afd2b85c208142a0f4cd8b7b
Sadie <stac0100@algonquinlive.com>
2022-12-08 19:55:26 -0500

    giving up for tonight

--- script.js
+++ script.js
@@ -33,3 +33,38 @@ const x = document.getElementById('x')
 x.addEventListener('click', function(){
   $nav.classList.remove('show')
 })
+
+// adding pokemon to homepage 
+const $pokemonGrid = document.getElementById('pokemonGrid')
+const pokemon = []
+
+// adding pokemon to myPokemon 
+
+const $myPokemonGrid = document.getElementById('myPokemonGrid')
+const myPokemon = []
+
+// might need to change to a for of loop?
+// needs ${i} to show different images 
+for (let i = 0; i < 19; i++) {
+    pokemon.push(`<div>
+    <div class="mainPokemon">
+        <div><img src="images/001.png" alt="pokemon"></div>
+        <span>#001</span>
+    </div>
+    <div class="pokemonContent">
+        <h2>Bulbasaur</h2>
+        <div>
+            <button class="grass">Grass</button>
+            <button class="poison">Poison</button>
+        </div>
+      </div>
+    </div>`)
+    myPokemon.push(`<div class="pokem">
+  <img class="responsive" src="images/001.png" alt="pokemon">
+  </div>`)
+}
+// adding pokemon to HTML
+$pokemonGrid.innerHTML = pokemon.join('')
+
+// adding myPokemon to HTML
+$myPokemonGrid.innerHTML = myPokemon.join('')
